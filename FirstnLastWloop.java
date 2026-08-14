3e16f83eb7542e1b46fb98e55acc1bd022b392a9
Bhoomika D <bhoomikabhoomi1620@gmail.com>
2026-08-14 22:06:15 +0530

    FirstnLastDigit

---
+++ FirstnLastWloop.java
@@ -0,0 +1,35 @@
+// Java Program to find first and last
+// digits of a number
+import java.lang.*;
+
+public class FirstnLastWloop{
+    
+    // Find the first digit
+    public static int FirstnLastWloop(int n)
+    {
+        // Remove last digit from number
+        // till only one digit is left
+        while (n >= 10) 
+            n /= 10;
+    
+        // return the first digit
+        return n;
+    }
+
+    // Find the last digit
+    public static int lastDigit(int n)
+    {
+        // return the last digit
+        return (n % 10);
+    }
+    
+    // driver function
+    public static void main(String argc[])
+    {
+        int n = 98562;
+        System.out.println(FirstnLastWloop(n) + " "
+        + lastDigit(n));
+    }
+}
+
+// This code is contributed by Sagar Shukla
