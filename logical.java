9b4727f0310c853d7c8fca5289dc786a56f93c1c
Bhoomika D <bhoomikabhoomi1620@gmail.com>
2026-08-19 22:43:35 +0530

    logical

---
+++ logical.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class logical {
+    public static void main(String[]args) {
+
+        boolean a=true;
+        boolean b=false;
+
+        boolean r=a&&b;
+        System.out.println(r);
+    }
+}
