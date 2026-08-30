fcfc01e1db0d8cb274ff8b1c24cd91528f04fd97
Bhoomika D <bhoomikabhoomi1620@gmail.com>
2026-08-30 21:38:11 +0530

    added OddEven prog

---
+++ OddEven.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+class OddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        if((n%2)==1){
+            System.out.println("You");
+        }else{
+            System.out.println("Friend");
+        }
+
+        sc.close();
+    }
+}
---
+++ sumofNformulae.java
@@ -0,0 +1,7 @@
+public class sumofNformulae {
+    public static void main(String[] args){
+        int N=5;
+        int sum=(N*(N-1))/2;
+        System.out.println("sum is " + sum);
+    }
+}
