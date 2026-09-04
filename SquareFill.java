1a1b56ec474873cf8e0697854387ec4819383bfb
Bhoomika D <bhoomikabhoomi1620@gmail.com>
2026-09-04 22:28:06 +0530

    Patterns

--- SquareFill.java
+++ SquareFill.java
@@ -2,27 +2,15 @@ import java.util.*;
 
 public class SquareFill {
 
-    public static void printPattern(int n)
-    {
-        int i, j;
+    public static void main(String [] args){
+        Scanner sc= new Scanner (System.in);
+        System.out.print("Enter n= ");
+        int n =sc.nextInt();
 
-        // outer loop to handle rows
-        for (i = 0; i < n; i++) {
-
-            // inner loop to handle columns
-            for (j = 0; j < n; j++) {
-                System.out.print("*");
-            }
-
-            // printing new line for each row
-            System.out.println();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("* ");
+            }System.out.println();
         }
-    }
-
-    // Driver Function
-    public static void main(String args[])
-    {
-        int n = 6;
-        printPattern(n);
     }
 }
---
+++ TriangleStarPyramid.java
@@ -0,0 +1,18 @@
+public class TriangleStarPyramid {
+    public static void Patternsetter(int n){
+
+        int i , j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++){
+                System.out.print("* ");
+            }System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        int n=6;
+        Patternsetter(n);
+    }
+}
